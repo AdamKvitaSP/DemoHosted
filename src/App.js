ccec82c41a5b27efd8e8c668ed6041dac25041cc
Adam Kvita <adamkvita@Adams-MacBook-Pro.local>
2021-02-10 14:09:44 +0100

    updated App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from the <em>master</em> branch!
+          Hello from the even nicer <em>master</em> branch!
         </p>
       </header>
     </div>
